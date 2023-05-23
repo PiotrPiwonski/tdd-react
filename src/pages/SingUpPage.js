fbf30ef2d16792b03a0404e829a22417faa1f24f
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-05-23 17:12:36 +0200

    add layout - Sing Up form and tests.

--- src/pages/SingUpPage.js
+++ src/pages/SingUpPage.js
@@ -3,7 +3,20 @@ import { Component} from "react";
 
 class SingUpPage extends Component {
     render() {
-        return <h1>Sing Up</h1>
+        return (
+           <div>
+               <h1>Sing Up</h1>
+               <label htmlFor="username">Username</label>
+               <input id="username"/>
+               <label htmlFor="email">E-mail</label>
+               <input id="email"/>
+               <label htmlFor="password">Password</label>
+               <input id="password" type="password"/>
+               <label htmlFor="passwordRepeat">Password Repeat</label>
+               <input id="passwordRepeat" type="password"/>
+               <button disabled>Sing Up</button>
+           </div>
+        )
     }
 }
 
