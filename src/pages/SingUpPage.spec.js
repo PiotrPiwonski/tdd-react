06bbbc29d09a8c92a1920fcc745488d4801d86ea
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-05-29 16:39:57 +0200

    add mocking for axios, fetch and use mws library.

--- src/pages/SingUpPage.spec.js
+++ src/pages/SingUpPage.spec.js
@@ -1,8 +1,10 @@
 import SingUpPage from './SingUpPage';
 import {render, screen} from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
-import axios from "axios";
-
+// import axios from "axios";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import resolve from "resolve";
 
 describe('Sing Up Page', () => {
     describe('Layout', () => {
@@ -62,7 +64,16 @@ describe('Sing Up Page', () => {
             const button = screen.queryByRole("button", {name: "Sing Up"});
             expect(button).toBeEnabled();
         });
-        it("sends username, email and password to backend after clicking the button", () => {
+        it("sends username, email and password to backend after clicking the button", async () => {
+            let requestBody;
+            const server = setupServer(
+                rest.post("/api/1.0/users", (req, res, ctx) => {
+                    requestBody = req.body;
+                    return res(ctx.status(200))
+                })
+            );
+            server.listen();
+
             render(<SingUpPage/>);
             const usernameInput = screen.getByLabelText("Username");
             const emailInput = screen.getByLabelText("E-mail");
@@ -74,18 +85,30 @@ describe('Sing Up Page', () => {
             userEvent.type(passwordRepeatInput, "P4ssword");
             const button = screen.queryByRole("button", {name: "Sing Up"});
 
-            const mockFn = jest.fn();
-            axios.post = mockFn;
-
             userEvent.click(button);
 
-            const firstCallOfMockFunction = mockFn.mock.calls[0];
-            const body = firstCallOfMockFunction[1];
-            expect(body).toEqual({
+            //const mockFn = jest.fn();
+            // axios.post = mockFn; // for axios
+            // window.fetch = mockFn;
+            // userEvent.click(button);
+
+            // const firstCallOfMockFunction = mockFn.mock.calls[0];
+            // const body = firstCallOfMockFunction[1]; // for axios
+            // const body = JSON.parse(firstCallOfMockFunction[1].body);
+            // expect(body).toEqual({
+            //     username: "user1",
+            //     email: "user1@mail.com",
+            //     password: "P4ssword",
+            // });
+
+            await new Promise((resolve) => setTimeout(resolve, 500));
+
+            expect(requestBody).toEqual({
                 username: "user1",
                 email: "user1@mail.com",
                 password: "P4ssword",
             });
+
         });
 
     });
